b7fd8498e7a09842b1afde3b8afaac04e41d82ab
Panha <yoeunsopanha2001@gmail.com>
2021-11-14 15:40:29 +0700

    add keychain library

---
+++ iOSKeychainDemo/Controller/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  iOSKeychainDemo
+//
+//  Created by sossokleng on 14/11/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func login(_ sender: Any) {
+    }
+    
+}
---
+++ iOSKeychainDemo/Controller/RegisterViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RegisterViewController.swift
+//  iOSKeychainDemo
+//
+//  Created by sossokleng on 14/11/21.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+ 
+    @IBAction func register(_ sender: Any) {
+    }
+    
+}
